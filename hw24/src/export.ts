addd7e3eaeffd86bf02ca82912ae87ba38aa9213
Pavlo Panukhnyk <panukhnykpa@gmail.com>
2025-07-14 20:33:58 +0200

    init hw 24

--- hw24/src/export.ts
+++
@@ -1,5 +0,0 @@
-export * from "./work1";
-export * from "./work2";
-export * from "./work3";
-export * from "./work4";
-
--- hw24/src/work1.ts
+++
@@ -1,8 +0,0 @@
-export function capitalize(str: string): string {
-  if (!str) return "";
-  return str[0].toUpperCase() + str.slice(1);
-}
-
-export function reverseString(str: string): string {
-  return str.split('').reverse().join('');
-}
